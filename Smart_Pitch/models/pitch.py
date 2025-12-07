470c8f59edc3a1673c92545da9cae4ad2a321109
Khaled Ameen <s-khaled.ameen@zewailcity.edu.eg>
2025-12-07 03:46:41 +0200

    Updated pitch.py

--- Smart_Pitch/models/pitch.py
+++ Smart_Pitch/models/pitch.py
@@ -1,1 +1,12 @@
-# Pitch Model
+from . import db  
+
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def _repr_(self):
+        return f"<Pitch {self.name}>"
