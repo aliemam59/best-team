3ad77cda98182aff023c8ebfcc8195a88a6449d0
Kh4led-05 <s-khaled.ameen@zewailcity.edu.eg>
2025-12-21 02:28:46 +0200

    Update user.py

--- Smart_Pitch/models/user.py
+++ Smart_Pitch/models/user.py
@@ -1,20 +1,21 @@
-from database.db import get_db
-
 class User:
+    users = [
+        {"id": 1, "email": "user@demo.com", "password": "123", "role": "user"},
+        {"id": 2, "email": "owner@demo.com", "password": "123", "role": "owner"},
+        {"id": 3, "email": "admin@demo.com", "password": "123", "role": "admin"},
+    ]
 
     @staticmethod
-    def create(name, email, password):
-        db = get_db()
-        db.execute(
-            "INSERT INTO users (full_name, email, password, role) VALUES (?, ?, ?, ?)",
-            (name, email, password, "user")
-        )
-        db.commit()
+    def login(email, password):
+        for user in User.users:
+            if user["email"] == email and user["password"] == password:
+                return user
+        return None
 
     @staticmethod
-    def get_by_login(email, password):
-        db = get_db()
-        return db.execute(
-            "SELECT * FROM users WHERE email=? AND password=?",
-            (email, password)
-        ).fetchone()
+    def get_all():
+        return User.users
+
+    @staticmethod
+    def delete(user_id):
+        User.users = [u for u in User.users if u["id"] != user_id]
